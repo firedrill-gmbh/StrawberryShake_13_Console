63570d9539935450c7fb37e13b894d80bc12de54
Markus Wellmann <markus.wellmann@firedrill.de>
2022-11-20 22:03:14 +0100

    Added Sample Code

---
+++ Demo/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using StrawberryShake;
+
+namespace Demo
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection
+                .AddConferenceClient()
+                .ConfigureHttpClient(client => client.BaseAddress = new Uri("https://workshop.chillicream.com/graphql"));
+
+            IServiceProvider services = serviceCollection.BuildServiceProvider();
+
+            IConferenceClient client = services.GetRequiredService<IConferenceClient>();
+
+            var result = await client.GetSessions.ExecuteAsync();
+            result.EnsureNoErrors();
+
+            foreach (var session in result.Data.Sessions.Nodes)
+            {
+                Console.WriteLine(session.Title);
+            }
+        }
+    }
+}
